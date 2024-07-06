1cd38fec3a409d9df74abce41ac473691439ed5e
Jon Williams <j.williams.softdev@gmail.com>
2024-07-06 18:53:01 +0000

    added a color change feature to the changing icons

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    // changing icon under header
+
+    // different icons
     const rpsIcons = [
         "fa-regular fa-hand-rock",
         "fa-regular fa-hand",
@@ -6,12 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
         "fa-regular fa-hand-lizard",
         "fa-regular fa-hand-spock"
     ];
+    // colors for the changing icon
+    const iconColors = [
+        '#f40234', 
+        '#f0882f',
+        '#ffe600',
+        '#63a000',
+        '#0059a1'
+    ]
 
     let currentIndex = 0;
-
+    // function to change icons and colors
     function changeIcons() {
         let icons = document.getElementById("changing-icon");
         icons.className = rpsIcons[currentIndex];
+        icons.style.color = iconColors[currentIndex];
         currentIndex = (currentIndex + 1) % rpsIcons.length;
     }
 
